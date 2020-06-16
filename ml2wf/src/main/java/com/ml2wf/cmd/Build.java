3302fdf32c4cff30b1080fd194177c58ba67ee2f
NicolasLACROIX <35873534+NicolasLacroix@users.noreply.github.com>
2020-06-16 16:25:55 +0200

    #84 Adding Build structure

---
+++ ml2wf/src/main/java/com/ml2wf/cmd/Build.java
@@ -0,0 +1,89 @@
+package com.ml2wf.cmd;
+
+import java.nio.file.Path;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import com.ml2wf.merge.WFMerger;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+/**
+ * Class managing the <b>save</b> command.
+ *
+ * <p>
+ *
+ * It calls required methods for saving the current workflow instance as a
+ * global task
+ * into the FeatureModel.
+ *
+ * <p>
+ *
+ * It is an extension of the {@link AbstractCommand} base class.
+ *
+ * @author Nicolas Lacroix
+ *
+ * @version 1.0
+ *
+ * @see AbstractCommand
+ * @see WFMerger
+ * @see Command
+ * @see Logger
+ *
+ */
+@Command(name = "build", version = "1.0", sortOptions = false, usageHelpWidth = 60, description = "build a knowledge tree (FeatureModel) from a set of meta-workflows and instance workflows")
+public class Build extends AbstractCommand {
+
+	@Option(names = { "-f",
+			"--feature" }, required = true, arity = "2", order = 1, description = "FeatureModel directory")
+	Path featureModel;
+
+	@Option(names = { "-m",
+			"--meta" }, required = true, arity = "2", order = 1, description = "meta-workflows directory")
+	Path metaDirectory;
+
+	@Option(names = { "-i",
+			"--instance" }, required = true, arity = "2", order = 1, description = "instance-workflows directory")
+	Path instanceDirectory;
+
+	@Option(names = { "-b",
+			"--backup" }, arity = "0", order = 1, description = "backup the original FeatureModel file before any modification")
+	boolean backUp;
+
+	/**
+	 * {@code WFMerger}'s instance.
+	 */
+	private WFMerger merger;
+
+	/**
+	 * Logger instance.
+	 *
+	 * @since 1.0
+	 * @see Logger
+	 */
+	private static final Logger logger = LogManager.getLogger(Build.class);
+
+	private void processMeta() {
+
+	}
+
+	private void processInstances() {
+
+	}
+
+	@Override
+	public void run() {
+		// TODO: process Merge verifications
+		Configurator.setLevel(getPackageName(), getVerbLevel(this.verbose));
+		try {
+			// TODO-
+			LogManager.shutdown();
+		} catch (Exception e) {
+			logger.fatal("Can't merge the Workflow with the FeatureModel.");
+			logger.fatal(e.getMessage());
+		}
+	}
+}
