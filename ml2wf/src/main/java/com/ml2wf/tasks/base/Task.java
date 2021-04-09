79f47d0aeebe8e157fd22559cd05ebd692f76363
NicolasLACROIX <35873534+NicolasLacroix@users.noreply.github.com>
2021-04-09 16:18:46 +0200

    #165 Cleaning tasks.base package

--- ml2wf/src/main/java/com/ml2wf/tasks/base/Task.java
+++ ml2wf/src/main/java/com/ml2wf/tasks/base/Task.java
@@ -2,11 +2,13 @@ package com.ml2wf.tasks.base;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.w3c.dom.Node;
 
-import com.ml2wf.conflicts.exceptions.UnresolvedConflict;
 import com.ml2wf.tasks.exceptions.InvalidTaskException;
 import com.ml2wf.tasks.specs.Spec;
 
@@ -18,256 +20,181 @@ import com.ml2wf.tasks.specs.Spec;
  * More precisely, it only contains uselful {@code Node}'s data and is more easy
  * to use than a classic {@code Node}.
  *
- *
  * @author Nicolas Lacroix
  *
  * @param <T> Any {@code class} implementing the {@code Spec interface}
  *
- * @version 1.0
- *
  * @see Node
  *
+ * @since 1.0.0
  */
 public abstract class Task<T extends Spec<?>> {
 
-	/**
-	 * Task's name.
-	 */
-	protected String name;
-	/**
-	 * The task's {@code Node} instance.
-	 */
-	protected Node node;
-	/**
-	 * Abstract status.
-	 */
-	protected boolean isAbstract;
-	/**
-	 * {@code Map} containing the current task specifications.
-	 */
-	protected Map<String, String> specs;
+    /**
+     * Task's name.
+     */
+    @Getter protected String name;
+    /**
+     * The task's {@code Node} instance.
+     */
+    @Getter @Setter protected Node node;
+    /**
+     * Abstract status.
+     */
+    @Getter @Setter protected boolean isAbstract;
+    /**
+     * {@code Map} containing the current task specifications.
+     */
+    @Getter protected Map<String, String> specs;
 
-	/**
-	 * {@code Task}'s full constructor.
-	 *
-	 * <p>
-	 *
-	 * It initializes a {@code Task} specifying its {@code name}, {@code parent},
-	 * {@code node} and {@code isAbstract} status.
-	 *
-	 * @param name       name of the task
-	 * @param node       node of the task
-	 * @param isAbstract whether the task is abstract or not
-	 * @throws InvalidTaskException
-	 */
-	public Task(String name, Node node, boolean isAbstract) {
-		if (name == null) {
-			throw new InvalidTaskException("Can't create a task with a nullable name");
-		}
-		this.name = name;
-		this.node = node;
-		this.isAbstract = isAbstract;
-		this.specs = new HashMap<>();
-	}
+    /**
+     * {@code Task}'s full constructor.
+     *
+     * <p>
+     *
+     * It initializes a {@code Task} specifying its {@code name}, {@code parent},
+     * {@code node} and {@code isAbstract} status.
+     *
+     * @param name       name of the task
+     * @param node       node of the task
+     * @param isAbstract whether the task is abstract or not
+     */
+    protected Task(String name, Node node, boolean isAbstract) {
+        if (name == null) {
+            throw new InvalidTaskException("Can't create a task with a nullable name");
+        }
+        this.name = name;
+        this.node = node;
+        this.isAbstract = isAbstract;
+        this.specs = new HashMap<>();
+    }
 
-	/**
-	 * {@code Task}'s partial constructor.
-	 *
-	 * <p>
-	 *
-	 * It initializes a {@code Task} specifying its {@code name}, {@code parent} and
-	 * {@code node}.
-	 *
-	 * @param name name of the task
-	 * @param node node of the task
-	 * @throws InvalidTaskException
-	 * @throws UnresolvedConflict
-	 */
-	public Task(String name, Node node) {
-		if (name == null) {
-			throw new InvalidTaskException("Can't create a task with a nullable name");
-		}
-		this.name = name;
-		this.node = node;
-		this.isAbstract = false;
-		this.specs = new HashMap<>();
-	}
+    /**
+     * {@code Task}'s partial constructor.
+     *
+     * <p>
+     *
+     * It initializes a {@code Task} specifying its {@code name}, {@code parent} and
+     * {@code node}.
+     *
+     * @param name name of the task
+     * @param node node of the task
+     */
+    protected Task(String name, Node node) {
+        if (name == null) {
+            throw new InvalidTaskException("Can't create a task with a nullable name");
+        }
+        this.name = name;
+        this.node = node;
+        this.isAbstract = false;
+        this.specs = new HashMap<>();
+    }
 
-	/**
-	 * Returns the current task's {@code name}.
-	 *
-	 * @return the current task's {@code name}
-	 */
-	public String getName() {
-		return this.name;
-	}
+    /**
+     * Returns the given {@code spec} value contained in the current task's
+     * specifications {@code Map} or {@code null} if it is not found.
+     *
+     * <p>
+     *
+     * <b>Note</b> that this method behaves like the {@link Map#get(Object)}
+     * method.
+     *
+     * @param spec specification to retrieve
+     *
+     * @return the current task's specifications {@code Map}
+     *
+     * @see Spec
+     */
+    public String getSpecValue(String spec) {
+        return this.specs.get(spec);
+    }
 
-	/**
-	 * Returns whether the current task {@link #isAbstract} or not.
-	 *
-	 * @return whether the current task is abstract or not
-	 */
-	public boolean isAbstract() {
-		return this.isAbstract;
-	}
+    /**
+     * Adds the given {@code spec} associated to its given {@code value} to the
+     * current task's specifications {@code Map}.
+     *
+     * <p>
+     *
+     * <b>Note</b> that this method behaves like the {@link Map#put(Object, Object)}
+     * method.
+     *
+     * @param spec  specification to add
+     * @param value the {@code spec} value
+     *
+     * @see Spec
+     */
+    public void addSpec(String spec, String value) {
+        this.specs.put(spec, value);
+    }
 
-	/**
-	 * Sets whether the current task's is abstract or not.
-	 *
-	 * @param isAbstract the new task's abstract status
-	 */
-	public void setAbstract(boolean isAbstract) {
-		this.isAbstract = isAbstract;
-	}
+    /**
+     * Adds the given {@code specs} to the current task's specifications
+     * {@code Map}.
+     *
+     * <p>
+     *
+     * <b>Note</b> that this method behaves like the {@link Map#putAll(Map)}
+     * method.
+     *
+     * @param specs specifications to add
+     *
+     * @see Spec
+     */
+    public void addAllSpecs(Map<String, String> specs) {
+        this.specs.putAll(specs);
+    }
 
-	/**
-	 * Returns the current task's {@link #node}.
-	 *
-	 * @return the current task's {@code node}
-	 */
-	public Node getNode() {
-		return this.node;
-	}
+    /**
+     * Applies the current specifications to the current {@link #node}.
+     */
+    public abstract void applySpecs();
 
-	/**
-	 * Sets the current task's {@link #node}.
-	 *
-	 * @param node the new task's {@code node}
-	 */
-	public void setNode(Node node) {
-		this.node = node;
-	}
+    /**
+     * Appends the given {@code child} to the current task.
+     *
+     * @param child task to append as child
+     *
+     * @return the appended child
+     *
+     * @see Task
+     */
+    public abstract Task<T> appendChild(Task<T> child);
 
-	/**
-	 * Returns the current task's specifications {@code Map}.
-	 *
-	 * @return the current task's specifications {@code Map}
-	 *
-	 * @since 1.0
-	 * @see Spec
-	 */
-	public Map<String, String> getSpecs() {
-		return this.specs;
-	}
+    /**
+     * Removes the given {@code oldChild} from the current {@code node}'s children
+     * list.
+     *
+     * @param oldChild  the child to remove
+     *
+     * @return an {@code Optional} containing the removed {@code oldChild} if it was
+     *         a current {@code node}'s child, an empty one else.
+     *
+     * @see Node
+     */
+    public abstract Optional<Task<T>> removeChild(Task<T> oldChild);
 
-	/**
-	 * Returns the given {@code spec} value contained in the current task's
-	 * specifications {@code Map} or {@code null} if it is not found.
-	 *
-	 * <p>
-	 *
-	 * <b>Note</b> that this method behaves like the {@link Map#get(Object)}
-	 * method.
-	 *
-	 * @param spec specification to retrieve
-	 * @return the current task's specifications {@code Map}
-	 *
-	 * @since 1.0
-	 * @see Spec
-	 */
-	public String getSpecValue(String spec) {
-		return this.specs.get(spec);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task<?> task = (Task<?>) o;
+        return isAbstract == task.isAbstract &&
+                Objects.equals(name, task.name) &&
+                Objects.equals(node, task.node) &&
+                Objects.equals(specs, task.specs);
+    }
 
-	/**
-	 * Adds the given {@code spec} associated to its given {@code value} to the
-	 * current task's specifications {@code Map}.
-	 *
-	 * <p>
-	 *
-	 * <b>Note</b> that this method behaves like the {@link Map#put(Object, Object)}
-	 * method.
-	 *
-	 * @param spec  specification to add
-	 * @param value the {@code spec} value
-	 *
-	 * @since 1.0
-	 * @see Spec
-	 */
-	public void addSpec(String spec, String value) {
-		this.specs.put(spec, value);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, node, isAbstract, specs);
+    }
 
-	/**
-	 * Adds the given {@code specs} to the current task's specifications
-	 * {@code Map}.
-	 *
-	 * <p>
-	 *
-	 * <b>Note</b> that this method behaves like the {@link Map#putAll(Map)}
-	 * method.
-	 *
-	 * @param specs specifications to add
-	 *
-	 * @since 1.0
-	 * @see Spec
-	 */
-	public void addAllSpecs(Map<String, String> specs) {
-		this.specs.putAll(specs);
-	}
-
-	/**
-	 * Applies the current specifications to the current {@link node}.
-	 */
-	public abstract void applySpecs();
-
-	/**
-	 * Appends the given {@code child} to the current task.
-	 *
-	 * @param child task to append as child
-	 * @return the appended child
-	 *
-	 * @since 1.0
-	 * @see Nod
-	 */
-	public abstract Task<T> appendChild(Task<T> child);
-
-	/**
-	 * Removes the given {@code oldChild} from the current {@code node}'s children
-	 * list.
-	 *
-	 * @param oldChild the child to remove
-	 * @return an {@code Optional} containing the removed {@code oldChild} if it was
-	 *         a current {@code node}'s child, an empty one else.
-	 *
-	 * @since 1.0
-	 * @see Node
-	 */
-	public abstract Optional<Task<T>> removeChild(Task<T> oldChild);
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = (prime * result) + (this.isAbstract ? 1231 : 1237);
-		result = (prime * result) + ((this.name == null) ? 0 : this.name.hashCode());
-		result = (prime * result) + ((this.node == null) ? 0 : this.node.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (this.getClass() != obj.getClass()) {
-			return false;
-		}
-		@SuppressWarnings("unchecked")
-		Task<T> other = (Task<T>) obj;
-		if (this.name == null) {
-			return this.name == other.name;
-		} else {
-			return this.name.equals(other.name);
-		}
-	}
-
-	@Override
-	public String toString() {
-		return "Task : " + this.getName();
-	}
-
+    @Override
+    public String toString() {
+        return String.format("%s : %s", getClass().getSimpleName(), name);
+    }
 }
--- ml2wf/src/main/java/com/ml2wf/tasks/base/WFTask.java
+++ ml2wf/src/main/java/com/ml2wf/tasks/base/WFTask.java
@@ -1,10 +1,14 @@
 package com.ml2wf.tasks.base;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.w3c.dom.Node;
 
 import com.ml2wf.conflicts.exceptions.UnresolvedConflict;
 import com.ml2wf.tasks.manager.TasksManager;
 import com.ml2wf.tasks.specs.Spec;
+
+import java.util.Objects;
 
 /**
  * This class represents a workflow {@code Task}.
@@ -23,72 +27,55 @@ import com.ml2wf.tasks.specs.Spec;
  */
 public abstract class WFTask<T extends Spec<?>> extends Task<T> {
 
-	/**
-	 * The name of the referred meta-task.
-	 */
-	protected String reference;
+    /**
+     * The name of the referred meta-task.
+     */
+    @Getter @Setter protected String reference;
 
-	/**
-	 * {@code WFTask}'s full constructor.
-	 *
-	 * <p>
-	 *
-	 * It initializes a {@code WFTask} specifying its {@code name} and
-	 * {@code reference}.
-	 *
-	 * @param name       name of the task
-	 * @param reference  reference of the task
-	 * @param node       node of the task
-	 * @param isAbstract whether the task is abstract or not
-	 * @throws UnresolvedConflict
-	 */
-	public WFTask(String name, Node node, boolean isAbstract, String reference)
-			throws UnresolvedConflict {
-		super(name, node, isAbstract);
-		this.reference = reference;
-		TasksManager.addTask(this); // add the new task to the manager
-	}
+    /**
+     * {@code WFTask}'s full constructor.
+     *
+     * <p>
+     *
+     * It initializes a {@code WFTask} specifying its {@code name} and
+     * {@code reference}.
+     *
+     * @param name       name of the task
+     * @param reference  reference of the task
+     * @param node       node of the task
+     * @param isAbstract whether the task is abstract or not
+     */
+    protected WFTask(String name, Node node, boolean isAbstract, String reference)
+            throws UnresolvedConflict {
+        super(name, node, isAbstract);
+        this.reference = reference;
+        // add the new task to the manager
+        TasksManager.addTask(this);
+    }
 
-	/**
-	 * Returns the current {@link #reference}.
-	 *
-	 * @return the current {@code reference}
-	 */
-	public String getReference() {
-		return this.reference;
-	}
+    @Override
+    public WFTask<T> appendChild(Task<T> child) {
+        child.setNode(this.node.appendChild(child.getNode()));
+        return (WFTask<T>) child;
+    }
 
-	/**
-	 * Sets the current {@link #reference}.
-	 *
-	 * @param reference the new task's reference
-	 */
-	public void setReference(String reference) {
-		this.reference = reference;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        WFTask<?> wfTask = (WFTask<?>) o;
+        return Objects.equals(reference, wfTask.reference);
+    }
 
-	@Override
-	public WFTask<T> appendChild(Task<T> child) {
-		child.setNode(this.node.appendChild(child.getNode()));
-		return (WFTask<T>) child;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = (prime * result) + ((this.reference == null) ? 0 : this.reference.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		return (obj instanceof WFTask) && super.equals(obj);
-	}
-
-	@Override
-	public String toString() {
-		return "WFTask : " + this.getName();
-	}
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), reference);
+    }
 }
