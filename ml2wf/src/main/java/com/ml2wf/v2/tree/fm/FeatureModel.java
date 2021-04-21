fba5850222de2ac72e1a8725b6c6e1dcf71b8388
NicolasLACROIX <35873534+NicolasLacroix@users.noreply.github.com>
2021-04-21 15:05:59 +0200

    #169 Delegating FM tree's operations to structure

--- ml2wf/src/main/java/com/ml2wf/v2/tree/fm/FeatureModel.java
+++ ml2wf/src/main/java/com/ml2wf/v2/tree/fm/FeatureModel.java
@@ -2,21 +2,30 @@ package com.ml2wf.v2.tree.fm;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import com.ml2wf.v2.tree.AbstractTree;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.Delegate;
 
 @JacksonXmlRootElement(localName = "extendedFeatureModel")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Setter(AccessLevel.PRIVATE)
 @ToString
-public class FeatureModel  {
+public class FeatureModel extends AbstractTree<FeatureModelTask> {
 
     @JacksonXmlProperty(localName = "struct")
-    @Delegate
     private FeatureModelStructure structure;
+
+    @Override
+    public FeatureModelTask appendChild(FeatureModelTask child) {
+        // delegates to structure
+        return structure.appendChild(child);
+    }
+
+    @Override
+    public FeatureModelTask removeChild(FeatureModelTask child) {
+        // delegates to structure
+        return structure.removeChild(child);
+    }
 }
