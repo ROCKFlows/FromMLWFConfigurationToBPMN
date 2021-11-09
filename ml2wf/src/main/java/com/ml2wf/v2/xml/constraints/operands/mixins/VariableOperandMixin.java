2c05fa2ed5a96901c7eea6872e6eceb1df045e9b
NicolasLACROIX <35873534+NicolasLacroix@users.noreply.github.com>
2021-11-09 22:27:11 +0100

    Moving XML-related stuff to dedicated package

--- ml2wf/src/main/java/com/ml2wf/v2/xml/constraints/operands/mixins/VariableOperandMixin.java
+++ ml2wf/src/main/java/com/ml2wf/v2/xml/constraints/operands/mixins/VariableOperandMixin.java
@@ -1,7 +1,7 @@
-package com.ml2wf.v2.constraints.operands.xml.mixins;
+package com.ml2wf.v2.xml.constraints.mixins;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.ml2wf.v2.constraints.operands.xml.serializer.VariableOperandSerializer;
+import com.ml2wf.v2.xml.constraints.serializer.VariableOperandSerializer;
 
 @JsonSerialize(using = VariableOperandSerializer.class)
 public interface VariableOperandMixin {
--- ml2wf/src/main/java/com/ml2wf/v2/xml/constraints/operands/serializer/VariableOperandSerializer.java
+++ ml2wf/src/main/java/com/ml2wf/v2/xml/constraints/operands/serializer/VariableOperandSerializer.java
@@ -1,4 +1,4 @@
-package com.ml2wf.v2.constraints.operands.xml.serializer;
+package com.ml2wf.v2.xml.constraints.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
