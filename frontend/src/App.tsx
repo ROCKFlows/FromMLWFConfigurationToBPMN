69151b851c51430a98b9270c5efb1360a11e337a
NicolasLACROIX <nicolacr06@gmail.com>
2023-01-03 23:59:58 +0100

    Adding workflow import in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import {AlertColor, Button, Snackbar, Stack, TextField, Typography} from "@mui/material";
+import {AlertColor, Button, Grid, Paper, Snackbar, Stack, TextField, Typography} from "@mui/material";
 import {Alert, TreeItem, TreeView} from "@mui/lab";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import {ReactElement, useEffect, useState} from "react";
 import axios from "axios";
 import {XMLParser} from "fast-xml-parser";
+import Dropzone from 'react-dropzone'
 
 function App() {
 
@@ -16,10 +17,7 @@ function App() {
     const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor | undefined>(undefined);
     const [snackbarMessage, setSnackbarMessage] = useState<string | undefined>(undefined);
 
-    useEffect(() => {
-        if (!version) {
-            return;
-        }
+    const getTree = () => {
         axios.get(`http://localhost:8080/ml2wf/api/v1/fm?versionName=${version}`)
             .then((r) => {
                 setKnowledgeTree(new XMLParser().parse(r.data));
@@ -33,6 +31,13 @@ function App() {
             .finally(() => {
                 setSnackbarOpen(true);
             });
+    }
+
+    useEffect(() => {
+        if (!version) {
+            return;
+        }
+        getTree();
     }, [version]);
 
     const getNodes = (node: any): ReactElement => {
@@ -40,19 +45,39 @@ function App() {
             return <TreeItem nodeId={node.name} label={node.name} />;
         }
         if (!node.name) {
-            return Array.isArray(node.children) ? node.children.map((c) => getNodes(c)) : getNodes(node.children);
+            return Array.isArray(node.children) ? node.children.map(getNodes) : getNodes(node.children);
         }
         return (
             <TreeItem nodeId={node.name} label={node.name}>
-                {Array.isArray(node.children) ? node.children.map((c) => getNodes(c)) : getNodes(node.children)}
+                {Array.isArray(node.children) ? node.children.map(getNodes) : getNodes(node.children)}
             </TreeItem>
         );
     }
 
+    const importWorkflow = (worflowFile: File) => {
+        const reader = new FileReader();
+        reader.onerror = () => {
+            setSnackbarSeverity('error');
+            setSnackbarMessage('Failed to import the workflow.');
+        }
+        reader.onload = () => {
+            axios.post(`http://localhost:8080/ml2wf/api/v1/bpmn?versionName=${version}`, reader.result, {
+                headers: {
+                    'Content-Type': 'application/xml'
+                }
+            }).then(() => {
+                setSnackbarSeverity('success');
+                setSnackbarMessage('Workflow successfully imported. Refreshing tree knowledge...');
+                getTree();
+            })
+        }
+        reader.readAsArrayBuffer(worflowFile);
+    }
+
     return (
-        <Stack>
+        <Stack height="100%">
             <Typography>ml2wf</Typography>
-            <Stack direction='row' padding={1}>
+            <Stack direction='row' padding={1} height="100%">
                 <TextField
                     label="Version"
                     variant="outlined"
@@ -66,13 +91,45 @@ function App() {
                     Confirm
                 </Button>
             </Stack>
-            <TreeView
-                aria-label="knowledge-tree"
-                defaultCollapseIcon={<ExpandMoreIcon />}
-                defaultExpandIcon={<ChevronRightIcon />}
-            >
-                {knowledgeTree && getNodes(knowledgeTree.FeatureModel.structure.children)}
-            </TreeView>
+            <Grid container spacing={2} height="100%">
+                <Grid item xs={6} height="100%">
+                    <Paper>
+                        <TreeView
+                            aria-label="knowledge-tree"
+                            defaultCollapseIcon={<ExpandMoreIcon />}
+                            defaultExpandIcon={<ChevronRightIcon />}
+                        >
+                            {knowledgeTree && getNodes(knowledgeTree.FeatureModel.structure.children)}
+                        </TreeView>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6} height="100%">
+                    <Paper style={{height: "50%"}}>
+                        <Stack height="50%" >
+                            <Typography>Constraints</Typography>
+                        </Stack>
+                    </Paper>
+                    <Paper>
+                        <Stack height="50%">
+                            <Typography>Workflows</Typography>
+                            <Dropzone
+                                multiple={false}
+                                disabled={!version}
+                                onDrop={(acceptedFiles) => importWorkflow(acceptedFiles[0])}
+                            >
+                                {({getRootProps, getInputProps}) => (
+                                    <section>
+                                        <div {...getRootProps()}>
+                                            <input {...getInputProps()} />
+                                            <p>Drag `&apos;`n`&apos;` drop some workflow file to import, or click to select file</p>
+                                        </div>
+                                    </section>
+                                )}
+                            </Dropzone>
+                        </Stack>
+                    </Paper>
+                </Grid>
+            </Grid>
             <Snackbar
                 open={isSnackbarOpen}
                 autoHideDuration={6000}
