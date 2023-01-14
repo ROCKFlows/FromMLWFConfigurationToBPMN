641a0a18cd64c5e130eae434cb1e57f614e4c180
NicolasLACROIX <nicolacr06@gmail.com>
2023-01-14 19:29:15 +0100

    Replacing arango by neo4j in deploy_poc.sh

--- deploy_poc.sh
+++ deploy_poc.sh
@@ -11,13 +11,10 @@ echo "Waiting 10 seconds to make sure everything is ready..."
 sleep 10
 echo "Done."
 
-echo "Importing feature model into arangodb..."
-curl -X POST "http://localhost:8080/ml2wf/api/v1/fm?versionName=demopoc" -H  "accept: application/json" -H  "Content-Type: application/xml" -d '@BPMN-Models/light_model_sample.xml'
+echo "Importing feature model into neo4j..."
+curl -X POST "http://localhost:8080/ml2wf/api/v1/fm?versionName=demo" -H  "accept: application/json" -H  "Content-Type: application/xml" -d '@model.xml'
 echo -e "\nDone."
 
 echo "To visualize the imported feature model:"
-echo "   - go to http://localhost:8529/"
-echo "   - username=root password=ml2wf_password"
-echo "   - choose ml2wf-database"
-echo "   - in collections, the FeatureModelTask collection contains the imported tasks"
-echo "   - to create the graph for visualization, go to graphs > Add graph, select a name and select input suggestions"
+echo "   - go to http://localhost:7474/browser/"
+echo "   - username=neo4j password=ml2wf_password"
