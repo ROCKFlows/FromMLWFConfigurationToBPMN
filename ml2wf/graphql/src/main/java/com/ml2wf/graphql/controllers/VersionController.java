8d384c198197f9dd711f9ff7715f5b4a3fbc848d
NicolasLACROIX <nicolacr06@gmail.com>
2023-01-30 21:23:14 +0100

    Adding graphql API for versions

---
+++ ml2wf/graphql/src/main/java/com/ml2wf/graphql/controllers/VersionController.java
@@ -0,0 +1,29 @@
+package com.ml2wf.graphql.controllers;
+
+import com.ml2wf.contract.business.IVersionsComponent;
+import com.ml2wf.core.tree.StandardKnowledgeVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class VersionController {
+
+    private final IVersionsComponent versionsComponent;
+
+    VersionController(@Autowired IVersionsComponent versionsComponent) {
+        this.versionsComponent = versionsComponent;
+    }
+
+    @QueryMapping
+    List<StandardKnowledgeVersion> versions() {
+        return versionsComponent.getVersions();
+    }
+
+    @QueryMapping
+    StandardKnowledgeVersion lastVersion() {
+        return versionsComponent.getLastVersion();
+    }
+}
--- ml2wf/graphql/src/main/java/com/ml2wf/graphql/exceptions/GraphQLExceptionResolver.java
+++ ml2wf/graphql/src/main/java/com/ml2wf/graphql/exceptions/GraphQLExceptionResolver.java
@@ -1,6 +1,7 @@
 package com.ml2wf.graphql.exceptions;
 
 import com.ml2wf.contract.exception.ConfigurationNotFoundException;
+import com.ml2wf.contract.exception.NoVersionFoundException;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
@@ -15,14 +16,24 @@ public class GraphQLExceptionResolver extends DataFetcherExceptionResolverAdapte
     // TODO: factorize with ExceptionHandlerController
     private static final String CONFIGURATION_NOT_FOUND_MESSAGE_PATTERN = "Configuration with name %s not found.";
     private static final String UNEXPECTED_ERROR = "Unexpected error while fetching data.";
+    private static final String NO_VERSION_FOUND_MESSAGE_PATTERN = "No version found.";
 
     @Override
     protected GraphQLError resolveToSingleError(@NonNull Throwable ex, @NonNull DataFetchingEnvironment env) {
-        if (ex instanceof ConfigurationNotFoundException configurationNotFoundException) {
+        // TODO: to improve
+        if (ex instanceof ConfigurationNotFoundException cnfe) {
             return GraphqlErrorBuilder.newError()
                     .errorType(ErrorType.NOT_FOUND)
                     .message(String.format(CONFIGURATION_NOT_FOUND_MESSAGE_PATTERN,
-                            configurationNotFoundException.getConfigurationName()))
+                            cnfe.getConfigurationName()))
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        if (ex instanceof NoVersionFoundException) {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(NO_VERSION_FOUND_MESSAGE_PATTERN)
                     .path(env.getExecutionStepInfo().getPath())
                     .location(env.getField().getSourceLocation())
                     .build();
